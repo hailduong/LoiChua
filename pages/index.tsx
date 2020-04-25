5be8c32fde77a9c63ad0fff882c010baf41d180e
LyDuong <ly.duong@navigosgroup.com>
2020-04-25 19:28:49 +0700

    update: add privacy policy page

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,9 @@ class Home extends React.PureComponent<any, any> {
     render() {
 
 
-        return <div className="container">
+        return <div className="container-fluid">
             <Head>
-                <title>Create Next App</title>
+                <title>Lời Chúa - Word Of God</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
